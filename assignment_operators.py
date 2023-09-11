c318c9400eab0c5dfd697a1398831a41b2a3ef3c
Angel199610 <kwagalaangel27@gmail.com>
2023-09-11 15:23:06 +0300

    "angel"

---
+++ assignment_operators.py
@@ -0,0 +1,52 @@
+#Question 1 (i)
+angel = (2,4,6)
+sum_of_angel = (2+4+6)
+print(sum_of_angel)
+print("sum of even numbers less than 8" +str(sum_of_angel))
+
+#Random numbers from keyboard
+angel = (6,5,7,8,9)
+sum_angel = (6+5+7+8+9)
+#print(sum_angel)
+Average = (sum(angel)/5)
+print(Average)
+print("|Average" + str(Average))
+
+#Operations
+#question a
+-5 + 8 * 6
+Purity = -5
+wonder = 8
+pure = 6
+peter = wonder*pure
+print(Purity+peter) 
+
+#question b
+(55+9) %9
+shakira = 55
+fat = 9
+kwa = (shakira + fat)
+print(kwa%fat)
+
+#question c
+20 + -3*5/8
+won = 20
+bea = -3
+lyra = 5
+jera = 8
+kal = (bea*lyra)
+jal = (kal/jera)
+print(won+jal)
+
+#question d
+5 + 15/3*2-8%3
+kal = 5
+num = 15
+frnd = 3
+exa = 2
+lar = 8
+ira = 3
+hara = (frnd*exa)
+mara = (num/hara)
+beau = (mara-lar)
+print(kal + beau)
